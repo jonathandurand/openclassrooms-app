3347b2dfea97a1f07f7f4f5589e54447a28adca5
jonathandurand <jonathan.durand25@gmail.com>
2025-07-03 14:21:20 +0000

    Creation de l'application et du fichier requirements.txt

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,86 @@
+import pandas as pd
+import numpy as np
+import json
+import boto3
 import streamlit as st
+import requests
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+global app_name
+global region
+app_name = 'my-deployment-attemp'
+region = 'us-east-1'
+
+global answers_tab
+answers_tab = ["sans risque", "à risque"]
+
+def check_status(app_name):
+    sage_client = boto3.client('sagemaker', region_name=region)
+    endpoint_description = sage_client.describe_endpoint(EndpointName=app_name)
+    endpoint_status = endpoint_description['EndpointStatus']
+    return endpoint_status
+
+def query_endpoint(app_name, input_json):
+	client = boto3.session.Session().client('sagemaker-runtime', region)
+
+	response = client.invoke_endpoint(
+		EndpointName = app_name,
+		Body = input_json,
+		ContentType = 'application/json'#'; format=pandas-split',
+		)
+
+	preds = response['Body'].read().decode('ascii')
+	preds = json.loads(preds)
+	#print('Received response: {}'.format(preds))
+	return preds
+
+
+def main():
+	MLFLOW_URI = 'http://127.0.0.1:5000/invocations'
+	
+	st.title('Prédiction du risque de crédit')
+	
+	status_btn = st.button('Status')
+	if status_btn:
+		st.write(
+            'Application status is {}'.format(check_status(app_name)))
+
+	ext_source_3 = st.number_input('Source extérieur (3)',
+                                 min_value=0., max_value=0.9, value=0.5, step=0.1)
+
+	ext_source_2 = st.number_input('Source extérieur (2)',
+                                 min_value=0., max_value=0.9, value=0.5, step=0.1)
+
+	amt_annuity_input = st.number_input('Montant annuel (en millier)',
+                                 min_value=2., max_value=250., value=25., step=1.)
+	amt_annuity = amt_annuity_input*1000.
+
+	birth_years = st.number_input('Age',
+                                 min_value=20., max_value=70., value=45., step=1.)
+
+	days_employed_input = st.number_input('Années en emploi',
+                                 min_value=0., max_value=50., value=5., step=1.)
+	days_employed = days_employed_input*(-365.)
+
+	days_last_phone_change_input = st.number_input('Dernier changement de téléphone (années)',
+                                 min_value=0., max_value=10., value=2., step=0.1)
+	days_last_phone_change = days_last_phone_change_input*(-365.)
+	 
+	predict_btn = st.button('Prédire')
+	if predict_btn:
+		arr_predict = [ext_source_3,
+						ext_source_2,
+						amt_annuity,
+						birth_years,
+						days_employed,
+						days_last_phone_change]
+		query_input = pd.DataFrame(arr_predict).transpose().to_dict(orient='split')
+		data = {"dataframe_split": query_input}
+		byte_data = json.dumps(data).encode('utf-8')
+		
+		pred = query_endpoint(app_name=app_name, input_json=byte_data)
+		
+		st.write('Le modèle prédit que le client est {}'.format(answers_tab[pred['predictions'][0]]))
+
+
+if __name__ == '__main__':
+    main()
